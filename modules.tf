0390bd35ef75b6e5813cf3ab3d8a2af200a2d351
Nikhil Vichare <vichnik@ad.issgovernance.com>
2020-12-18 12:03:47 +0530

    provider changes

--- modules.tf
+++ modules.tf
@@ -1,9 +1,13 @@
 terraform {
   required_version = "~> 0.13"
+  aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
 }
 
 provider "aws" {
-
+  profile = "default"
   access_key = "AKIA4WM6FXUN26ZVNFF7"
   secret_key = "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"
   region = var.region
