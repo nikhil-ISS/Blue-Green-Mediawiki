44b479b41b537de8112ebb5f4c0a1fa52d26ce05
Nikhil Vichare <vichnik@ad.issgovernance.com>
2020-12-18 12:31:31 +0530

    provider changes

--- modules.tf
+++ modules.tf
@@ -15,14 +15,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  profile = "default"
-  #access_key = "AKIA4WM6FXUN26ZVNFF7"
-  #secret_key = "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"
-  region = var.region
-}
-
-
 module "B-G" {
   source = "./B-G"
 }
